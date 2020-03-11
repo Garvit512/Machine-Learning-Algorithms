8c3dbacf5fe2fa7ac3e69f628eea64801ebbe7ba
Garvit512 <garvitarya512@gmail.com>
2020-03-11 22:17:14 +0530

    Multiple Regression using Normal Equation

--- Regression/Multiple_Regression.py
+++ Regression/Multiple_Regression.py
@@ -6,6 +6,11 @@ Created on Tue Mar 10 13:43:39 2020
 @author: garvit
 """
 
+# Multivariate Linear Regression from scratch
+'''
+y = m0x0 + m1x1 + m2x2 + ....... 
+'''
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
